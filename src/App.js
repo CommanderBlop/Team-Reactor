709111f102bd29ac38c71e78bc8f3c3faa342f75
CommanderBlop <kcajlong@outlook.com>
2020-09-05 15:22:58 +0800

    Conflicted line deletion

--- src/App.js
+++ src/App.js
@@ -3,33 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import FirebaseContext from './Firebase';
-import NavBar from './NavBar';
-
-function App() {
-	return (
-		<div className='App'>
-			<Router>
-				<NavBar />
-				<Switch>
-					<Route path='/'>
-						<h1>Home Page: Team Recator</h1>
-					</Route>
-				</Switch>
-			</Router>
-		</div>
-	);
-import MovieRec from './MovieCollection.js'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom"
-import FirebaseContext from './Firebase'
 import NavBar from './NavBar'
-
 import Home from './community-posts/home'
-
+import MovieCollection from "./MovieCollection"
 
 function App() {
   return (
