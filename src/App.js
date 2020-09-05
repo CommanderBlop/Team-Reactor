e15dd5c93221828dd3c0bd5a3d64f4c7dc025810
olinawu3w <70761465+olinawu3w@users.noreply.github.com>
2020-09-05 15:11:50 +0800

    conflict resolved

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-//test codes
-import CommunitiesMain from './Communities/MainCommunityPage';
-
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import FirebaseContext from './Firebase';
+import NavBar from './NavBar';
 
 function App() {
 	return (
 		<div className='App'>
-			<CommunitiesMain />
+			<Router>
+				<NavBar />
+				<Switch>
+					<Route path='/'>
+						<h1>Home Page: Team Recator</h1>
+					</Route>
+				</Switch>
+			</Router>
 		</div>
 	);
 }
