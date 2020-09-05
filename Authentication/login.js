eadae86aadf178908efde2bf13ee9fa9ddf8dc20
hzhfred <63511984+hzhfred@users.noreply.github.com>
2020-09-04 20:07:29 -0700

    add authentication

---
+++ Authentication/login.js
@@ -0,0 +1,28 @@
+import React from "react";
+import './login.css';
+
+
+function Login(props) {
+
+
+  return (
+
+    <div className="login-page">
+      <div className="form">
+
+
+        <form className="login-form" >
+          <input type="text" placeholder="username"/>
+          <input type="password" placeholder="password"/>
+          <button>login</button>
+          <p className="message">Not registered? <a href="#">Create an account</a></p>
+        </form>
+      </div>
+    </div>
+
+
+
+  );
+}
+
+export default Login;
