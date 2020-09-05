3fcaa978c3f853dd1f251f57305ff05baf7a64b4
CommanderBlop <kcajlong@outlook.com>
2020-09-05 22:34:48 +0800

    search page and broken upload function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect, useContext} from 'react'
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import FirebaseContext from './Firebase';
 import NavBar from './NavBar'
 import Home from './community-posts/home'
-import MovieCollection from "./MovieCollection"
+import MovieCollection from "./MovieCollection/MovieCollection"
 import Login from "./Authentication/login"
 import Register from './Authentication/Register'
+import Populator from './populateFirebase'
+import AddMovie from './MovieCollection/addMovie'
 
 
 function App() {
+  // const firebase = useContext(FirebaseContext)
+  //   firebase.db.collection("movieTester").doc("testUser").set({
+  //       movieID: ['tt4566758','tt6723592','tt8946378']
+  //   })
+  //     .then(function() {
+  //     console.log("Document successfully written!");
+  //   })
+  //     .catch(function(error) {
+  //         console.error("Error writing document: ", error);
+  //   });
+    
   return (
     <div className="App">
       <Router>
           <NavBar />
           <Switch>
-
 
             <Route path="/myCollection">
               <MovieCollection />
@@ -33,8 +45,12 @@ function App() {
             <Route path="/login">
               <Login />
             </Route>
-    <Route path="/register">
+            <Route path="/register">
               <Register/>
+            </Route>
+
+            <Route path="/addMovie">
+              <AddMovie/>
             </Route>
 
 
