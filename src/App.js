5f41ed4e14d7474f35295abd9b1b9c0f3cc3d675
yhuang4 <yhuang4@andrew.cmu.edu>
2020-09-05 11:02:25 +0800

    router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 import FirebaseContext from './Firebase'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Team Reactors</h1>
-      </header>
+      <Router>
+      <div>
+        <Switch>
+          <Route path="/">
+            <h1>Home Page: Team Recator</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     </div>
   );
 }
